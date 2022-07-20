bab82e667d41413250fe57b24a8381200b3cf14c
OreOluwa Faseru <orefash@gmail.com>
2022-07-20 02:00:33 +0100

    set up mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { CategoryModule } from './category/category.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, PostModule, CategoryModule],
+  imports: [
+    UserModule, 
+    PostModule, 
+    CategoryModule,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGO_URI,
+      database: process.env.MONGO_DB,
+      entities: [
+        __dirname + '/**/*.entity{.ts,.js}',
+      ],
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.entity.ts
@@ -0,0 +1,6 @@
+
+export class UserEntity { 
+    id: string;   
+    email: string;
+    password: string;
+}
